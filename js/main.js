23e0264cf268b7830de3d921e27a071df9c64f78
ITWebde <baxriddinovsamandar07@gmail.com>
2021-11-20 15:06:25 +0500

    Javascript 2-work imtihon answer

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// var music = ["Jazz", "Blues"];
+// console.log(music);
+// var musicRock = music.push("Rock-n-roll");
+// console.log(music);
+//  music.splice(1, 1, "Classic");
+// console.log(music);
+// var musicDelete = music.shift();
+// console.log(music);
+// var musicShift = music.unshift("Rap, Reggae,")
+// console.log(music);
+var typeMusic = ['Jazz', 'Blues'];
+console.log(typeMusic);
+
+var music_rock = typeMusic.push('Rock-n-roll');
+console.log(typeMusic);
+
+typeMusic.splice(1 , 1, 'Classic');
+console.log(typeMusic);
+
+var delete_music = typeMusic.shift();
+console.log(typeMusic);
+
+var shift_music = typeMusic.unshift('Rap, Reggae')
+console.log(typeMusic);
